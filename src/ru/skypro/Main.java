5aa33c2eabd8c507a434ee349a391c0a703ab75d
alekdersan <repka1795@gmail.com>
2022-01-17 23:43:16 +0300

    решил обязательные 4 задачи

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,27 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//	  задача 1
+        String firstName = "Семён" + " ";
+        String middleName = "Семёнович" + " ";
+        String lastName = "Иванов" + " ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+//    задача 2
+        String capLett = fullName.toUpperCase(Locale.ROOT);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + capLett);
+
+//    задача 3
+        String perCom = fullName.replace(' ', ';');
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + perCom);
+
+//    задача 4
+        String swap = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + swap);
     }
 }
